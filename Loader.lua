79e5e8ad77b6a72726239136fdec7a0af0ac5138
kylosilly <gangggevil@gmail.com>
2025-05-11 12:12:55 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,36 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 
+local games = "https://raw.githubusercontent.com/kylosilly/Astolfo-Ware-V2/main/Games"
+
+local local_player = cloneref(game:GetService("Players").LocalPlayer)
+local market = cloneref(game:GetService("MarketplaceService"))
+local info = market:GetProductInfo(game.PlaceId)
+
+local blacklisted_executors = {
+    "Solara",
+    "Xeno",
+    "Nezur",
+    "JJSploit"
+}
+
+function check_supported()
+    local success, result = pcall(function()
+        return game:HttpGet(games.."/"..game.PlaceId..".lua")
+    end)
+    if not success or result == "404: Not Found" then
+        library:Notify("Unsupported Game: "..info.Name.." If You Want It Supported Join The Discord Copied To Your Clipboard")
+        setclipboard("https://discord.gg/SUTpER4dNc")
+    end
+end
+
+if identifyexecutor() then
+    local executor = identifyexecutor()
+    if table.find(blacklisted_executors, executor) then
+        library:Notify("Unsupported Executor Use A Diffirent Executor")
+    end
+end
+
+check_supported()
+
+loadstring(game:HttpGet(games.."/"..game.PlaceId..".lua"))()
+library:Notify("Supported Game Loading: "..info.Name)
