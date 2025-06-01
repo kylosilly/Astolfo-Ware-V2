59825d0ee28bc7a58d6f1fcbaf2951fc22bd790c
kylosilly <183762182+kylosilly@users.noreply.github.com>
2025-06-01 18:59:05 +0200

    Update 119048529960596.lua

--- Games/119048529960596.lua
+++ Games/119048529960596.lua
@@ -1,4 +1,4 @@
---// If anyone is skidding or reading this mb for the ass optimizations </3
+-- uhm..............................?????
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
@@ -22,12 +22,17 @@ local tabs = {
     ["ui settings"] = window:AddTab("UI Settings")
 }
 
-local auto_group = tabs.main:AddLeftGroupbox("Auto Settings")
+local auto_group = tabs.main:AddLeftGroupbox("Auto Stuff")
+local auto_settings_group = tabs.main:AddRightGroupbox("Auto Settings")
+--[[
 local player_group = tabs.main:AddRightGroupbox("Player Settings")
 local misc_group = tabs.main:AddLeftGroupbox("Misc Settings")
+local restaurant_group = tabs.main:AddRightGroupbox("Restaurant Settings")
+]]
 local menu_group = tabs["ui settings"]:AddLeftGroupbox("Menu Settings")
 
 local marketplace_service = game:GetService("MarketplaceService")
+local user_input_service = game:GetService("VirtualInputManager")
 local replicated_storage = game:GetService("ReplicatedStorage")
 local virtual_user = game:GetService("VirtualUser")
 local run_service = game:GetService("RunService")
@@ -83,175 +88,108 @@ if not temp then
 end
 
 local auto_dirty_dish = false
-local auto_put_orders = false
 local auto_give_food = false
-local instant_food = false
+local auto_do_order = false
 local auto_order = false
-local auto_bills = false
-local auto_claim = false
-local fast_cook = false
-local auto_sit = false
-local auto_jar = false
+local auto_bill = false
+local auto_cook = false
+local auto_seat = false
+
+local toggle_delay = 0.1
+
+surface.DescendantAdded:Connect(function(v)
+    task.wait(1)
+    if auto_dirty_dish and v.Name == "Trash" and v:GetAttribute("Taken") then
+        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectDishes", FurnitureModel = v.Parent.Parent })
+    end
+
+    if auto_bill and v.Name == "Bill" then
+        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectBill", FurnitureModel = v.Parent })
+    end
+end)
 
 auto_group:AddDivider()
 
-auto_group:AddLabel("Must be near restaurant for features to work!", true)
+auto_group:AddLabel("Be near the restaurant for the features to work", true)
 
 auto_group:AddDivider()
-
-auto_group:AddToggle('auto_dirty_dish', {
-    Text = 'Auto Collect Dirty Dish',
-    Default = auto_dirty_dish,
-    Tooltip = 'Collects dirty dish on tables',
-
-    Callback = function(Value)
-        auto_dirty_dish = Value
-        if Value then
-            repeat
-                for _, v in next, surface:GetDescendants() do
-                    if v.Name == "Trash" and v:GetAttribute("Taken") then
-                        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectDishes", FurnitureModel = v.Parent.Parent })
-                    end
-                end
-                task.wait(.25)
-            until not auto_dirty_dish
-        end
-    end
-})
-
-auto_group:AddToggle('auto_sit', {
-    Text = 'Auto Seat Customers',
-    Default = auto_sit,
-    Tooltip = 'Automatically gives customers a seat',
-
-    Callback = function(Value)
-        auto_sit = Value
-        if Value then
-            repeat
-                for _, v in next, client_customers:GetChildren() do
-                    for _, v2 in next, surface:GetChildren() do
-                        if v2.Name:find("T") and not v2:GetAttribute("InUse") and v:IsA("Folder") then
-                            replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ GroupId = tostring(v.Name), Tycoon = tycoon, Name = "SendToTable", Furniture = v2 })
-                        end
-                    end
-                end
-                task.wait(1)
-            until not auto_sit
-        end
-    end
-})
-
-auto_group:AddToggle('auto_jar', {
-    Text = 'Auto Collect Tip Jar',
-    Default = auto_jar,
-    Tooltip = 'Collects tip jars',
-
-    Callback = function(Value)
-        auto_jar = Value
-        if Value then
-            repeat
-                for _, v in next, furniture:GetDescendants() do
-                    if v.Name:find("Jar") and v.Parent:GetAttribute("Value") > 0 then
-                        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TipsCollected"):FireServer(tycoon)
-                    end
-                end
-                task.wait(.25)
-            until not auto_jar
-        end
-    end
-})
-
-auto_group:AddToggle('auto_bills', {
-    Text = 'Auto Collect Bills',
-    Default = auto_bills,
-    Tooltip = 'Auto collects bills from tables',
-
-    Callback = function(Value)
-        auto_bills = Value
-        if Value then
-            repeat
-                for _, v in next, surface:GetDescendants() do
-                    if v.Name == "Bill" then
-                        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectBill", FurnitureModel = v.Parent })
-                    end
-                end
-                task.wait(.25)
-            until not auto_bills
-        end
-    end
-})
 
 auto_group:AddToggle('auto_order', {
-    Text = 'Auto Take Orders',
+    Text = 'Auto Take Customer Orders',
     Default = auto_order,
-    Tooltip = 'Automatically takes orders from customers',
+    Tooltip = 'Collects dirty dish on tables',
 
     Callback = function(Value)
         auto_order = Value
         if Value then
             repeat
-                for _, v in next, client_customers:GetDescendants() do
-                    if v:IsA("Model") then
-                        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ GroupId = tostring(v.Parent.Name), Tycoon = tycoon, Name = "TakeOrder", CustomerId = tostring(v.Name) })
+                for _, v in next, local_player.PlayerGui:GetDescendants() do
+                    if v:IsA("ImageLabel") and v.Visible and v.Parent and v.Parent.Parent.Parent.Name == "CustomerSpeechUI" then
+                        replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ GroupId = tostring(v.Parent.Parent.Parent.Adornee.Parent.Parent.Name), Tycoon = tycoon, Name = "TakeOrder", CustomerId = tostring(v.Parent.Parent.Parent.Adornee.Parent.Name) })
+                        task.wait()
                     end
                 end
-                task.wait(.5)
+                task.wait(toggle_delay)
             until not auto_order
         end
     end
 })
 
-auto_group:AddToggle('auto_claim', {
-    Text = 'Auto Claim Food',
-    Default = auto_claim,
-    Tooltip = 'Auto collects food after done with reward',
+auto_group:AddToggle('auto_dirty_dish', {
+    Text = 'Auto Collect Dirty Dish',
+    Default = auto_dirty_dish,
+    Tooltip = 'Collects dirty dishes on tables',
 
     Callback = function(Value)
-        auto_claim = Value
+        auto_dirty_dish = Value
         if Value then
-            repeat
-                if local_player.PlayerGui:FindFirstChild("Objectives"):FindFirstChild("Frame"):FindFirstChild("CollectRewardButton").Visible then
-                    replicated_storage:WaitForChild("Events"):WaitForChild("Rewards"):WaitForChild("RewardRequested"):FireServer()
+            for _, v in next, surface:GetDescendants() do
+                if v.Name == "Trash" and v:GetAttribute("Taken") then
+                    replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectDishes", FurnitureModel = v.Parent.Parent })
                 end
-                task.wait(2)
-                if local_player.PlayerGui:FindFirstChild("SelectReward"):FindFirstChild("Frame").Visible then
-                    for _, v in next, local_player.PlayerGui.SelectReward.Frame:FindFirstChild("List"):GetDescendants() do
-                        if v:IsA("ImageButton") then
-                            for _, v2 in next, (getconnections(v.MouseButton1Click)) do
-                                v2:Fire()
-                            end
-                        end
-                    end
-                end
-                task.wait(1)
-            until not auto_claim
+            end
         end
     end
 })
 
--- you can also just fire proximityprompt in temp but i decided to take the long way ahdhasdhashdahsda
-auto_group:AddToggle('auto_put_orders', {
-    Text = 'Auto Put Orders',
-    Default = auto_put_orders,
-    Tooltip = 'Puts orders in order counters',
+auto_group:AddToggle('auto_seat', {
+    Text = 'Auto Seat Customers',
+    Default = auto_seat,
+    Tooltip = 'Automatically seats customers',
 
     Callback = function(Value)
-        auto_put_orders = Value
+        auto_seat = Value
         if Value then
             repeat
-                if #temp:GetChildren() > 0 then
-                    for _, v in next, surface:GetDescendants() do
-                        if v.Name:find("Order") and v.Parent.Parent:GetAttribute("InUse") then
-                            for _, v2 in next, temp:GetDescendants() do
-                                if v2:IsA("ProximityPrompt") then
-                                    replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("Interactions"):WaitForChild("Interacted"):FireServer(tycoon, { WorldPosition = v.Parent.Parent:GetPivot().Position, Model = v.Parent.Parent, ActionText = "Cook", InteractionType = "OrderCounter", Part = v.Parent, Prompt = v, TemporaryPart = v.Parent, Id = v.Name })
-                                end
+                for _, v in next, local_player.PlayerGui:GetDescendants() do
+                    if v:IsA("TextLabel") and v.Text:find("table") and v.Parent and v.Parent.Parent.Parent.Name == "CustomerSpeechUI" then
+                        for _, v2 in next, surface:GetChildren() do
+                            if v2.Name:find("T") and not v2:GetAttribute("InUse") then
+                                replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ GroupId = tostring(v.Parent.Parent.Parent.Adornee.Parent.Parent.Name), Tycoon = tycoon, Name = "SendToTable", FurnitureModel = v2 })
+                                task.wait()
                             end
                         end
                     end
                 end
-                task.wait(.5)
-            until not auto_put_orders
+                task.wait(toggle_delay)
+            until not auto_seat
+        end
+    end
+})
+
+auto_group:AddToggle('auto_bill', {
+    Text = 'Auto Collect Bills',
+    Default = auto_bill,
+    Tooltip = 'Collects dirty dishes on tables',
+
+    Callback = function(Value)
+        auto_bill = Value
+        if Value then
+            for _, v in next, surface:GetDescendants() do
+                if v.Name == "Bill" then
+                    replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Tycoon = tycoon, Name = "CollectBill", FurnitureModel = v.Parent })
+                end
+            end
         end
     end
 })
@@ -265,110 +203,91 @@ auto_group:AddToggle('auto_give_food', {
         auto_give_food = Value
         if Value then
             repeat
-                if #food:GetChildren() > 0 then
-                    task.wait(.1)
+                if #food:GetChildren() > 0 and #client_customers:GetChildren() > 0 then
                     for _, v in next, food:GetChildren() do
-                        for _, v2 in next, client_customers:GetDescendants() do
-                            if v2:IsA("Model") and v:GetAttribute("Taken") then
-                                replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Name = "Serve", GroupId = tostring(v2.Parent.Name), Tycoon = tycoon, FoodModel = v, CustomerId = tostring(v2.Name) })
+                        for _, v2 in next, local_player.PlayerGui:GetDescendants() do
+                            if not v:GetAttribute("Taken") then
+                                replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("GrabFood"):InvokeServer(v)
+                                task.wait()
                             end
-                        end
-                        if not v:GetAttribute("Taken") then
-                            replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("GrabFood"):InvokeServer(v)
+                            if v2:IsA("ImageLabel") and v2.Visible and v2.Parent and v2.Parent.Parent.Parent.Name == "CustomerSpeechUI" then
+                                replicated_storage:WaitForChild("Events"):WaitForChild("Restaurant"):WaitForChild("TaskCompleted"):FireServer({ Name = "Serve", GroupId = tostring(v2.Parent.Parent.Parent.Adornee.Parent.Parent.Name), Tycoon = tycoon, FoodModel = v, CustomerId = tostring(v2.Parent.Parent.Parent.Adornee.Parent.Name) })
+                                task.wait()
+                            end
                         end
                     end
                 end
-                task.wait(.25)
+                task.wait(toggle_delay)
             until not auto_give_food
         end
     end
 })
 
-auto_group:AddToggle('instant_food', {
-    Text = 'Auto Make Food',
-    Default = instant_food,
-    Tooltip = 'Makes food very fast',
+auto_group:AddToggle('auto_do_order', {
+    Text = 'Auto Do Orders',
+    Default = auto_do_order,
+    Tooltip = 'Does orders for customers',
 
     Callback = function(Value)
-        instant_food = Value
+        auto_do_order = Value
         if Value then
             repeat
-                for _, v in next, surface:GetDescendants() do
-                    if v.Name:find("Oven") then
-                        replicated_storage:WaitForChild("Events"):WaitForChild("Cook"):WaitForChild("CookInputRequested"):FireServer("Interact", v.Parent, "Oven") -- this might not always be accurate to all ovens maybe idk
+                if #temp:GetChildren() > 0 then
+                    for _, v in next, temp:GetChildren() do
+                        if v:IsA("Part") and v:FindFirstChildOfClass("ProximityPrompt") then
+                            fireproximityprompt(v:FindFirstChildOfClass("ProximityPrompt"))
+                            task.wait()
+                        end
                     end
                 end
-                task.wait(.5)
-            until not instant_food
+                task.wait(toggle_delay)
+            until not auto_do_order
         end
     end
 })
 
-player_group:AddDivider()
+auto_group:AddToggle('auto_cook', {
+    Text = 'Auto Cook',
+    Default = auto_cook,
+    Tooltip = 'Automatically cooks food',
 
-player_group:AddButton({
-    Text = 'Anti Afk',
-    Func = function()
-        if get_gc then
-            for _, v in next, get_gc(local_player.Idled) do
-                if v["Disable"] then
-                    v["Disable"](v)
-                elseif v["Disconnect"] then
-                    v["Disconnect"](v)
+    Callback = function(Value)
+        auto_cook = Value
+        if Value then
+            local oven = nil
+            for _, v in next, surface:GetDescendants() do
+                if v.Name:find("Oven") then
+                    oven = v.Parent
+                    break
                 end
             end
-        else
-            local_player.Idled:Connect(function()
-                virtual_user:CaptureController()
-                virtual_user:ClickButton2(Vector2.new())
-            end)
+            repeat
+                if oven then
+                    replicated_storage:WaitForChild("Events"):WaitForChild("Cook"):WaitForChild("CookInputRequested"):FireServer("Interact", oven, "Oven")
+                elseif not oven then
+                    library:Notify("No Oven Found")
+                    return
+                end
+                task.wait(toggle_delay)
+            until not auto_cook
         end
-        library:Notify("Anti Afk Enabled!")
-    end,
-    DoubleClick = false,
-    Tooltip = 'Wont disconnect you after 20 minutes'
+    end
 })
 
-player_group:AddButton({
-    Text = 'Claim Daily Reward',
-    Func = function()
-        if not tycoon:FindFirstChild("Default"):FindFirstChild("DailyRewards") then
-            library:Notify("Already Claimed")
-            return
-        end
-        replicated_storage:WaitForChild("Events"):WaitForChild("DailyRewards"):WaitForChild("DailyRewardClaimed"):FireServer()
-        library:Notify("Claimed Daily Reward")
-    end,
-    DoubleClick = false,
-    Tooltip = 'Claims daily reward gift'
-})
+auto_settings_group:AddSlider('toggle_delay', {
+    Text = 'Toggle Repeat Delay',
+    Default = toggle_delay,
+    Min = 0,
+    Max = 5,
+    Rounding = 1,
+    Compact = false,
 
-misc_group:AddDivider()
-
-misc_group:AddButton({
-    Text = 'Destory Waypoints',
-    Func = function()
-        for _, v in next, temp:GetDescendants() do
-            if v.Name == "DirectionBeam" then
-                v.Parent:Destroy()
-            end
+    Callback = function(Value)
+        toggle_delay = Value
+        if Value == 0 then
+            library:Notify("0 Is Not Recommended As It May Cause Lag")
         end
-        library:Notify("Waypoints Destroyed")
-    end,
-    DoubleClick = false,
-    Tooltip = 'Destroys annoying waypoints'
-})
-
-misc_group:AddButton({
-    Text = 'Destroy Leftover Cooking Resources',
-    Func = function()
-        for _, v in next, tycoon.Objects.CookingResources[local_player.Name]:GetChildren() do
-            v:Destroy()
-        end
-        library:Notify("Leftover Cooking Resources Destroyed")
-    end,
-    DoubleClick = false,
-    Tooltip = 'Delete leftover cooking resources'
+    end
 })
 
 local frame_timer = tick()
@@ -392,15 +311,12 @@ end);
 
 menu_group:AddButton('Unload', function()
     auto_dirty_dish = false
-    auto_put_orders = false
     auto_give_food = false
-    instant_food = false
+    auto_do_order = false
     auto_order = false
-    auto_bills = false
-    auto_claim = false
-    fast_cook = false
-    auto_sit = false
-    auto_jar = false
+    auto_bill = false
+    auto_cook = false
+    auto_seat = false
     watermark_connection:Disconnect()
     library:Unload()
 end)
